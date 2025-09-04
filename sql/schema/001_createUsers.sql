48edf0d5c1973d0643c98c561daab110b9908409
Lewvy <aditya29december@gmail.com>
2025-09-04 21:38:51 +0530

    initial commit

---
+++ sql/schema/001_createUsers.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+create extension if not exists "uuid-ossp";
+create extension if not exists "citext";
+
+
+CREATE TABLE users (
+    id uuid PRIMARY KEY,
+	email citext unique not null,
+	name text not null,
+	password_hash bytea not null,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	version int not null default 1
+);
+
+-- +goose Down
+DROP TABLE users;
