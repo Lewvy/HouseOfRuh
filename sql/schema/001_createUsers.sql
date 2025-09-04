80c7ff00cd60f64ff8cc081690167cef6cbf35a0
Lewvy <aditya29december@gmail.com>
2025-09-04 21:41:06 +0530

    added phone number and address to the user table

--- sql/schema/001_createUsers.sql
+++ sql/schema/001_createUsers.sql
@@ -7,6 +7,8 @@ CREATE TABLE users (
     id uuid PRIMARY KEY,
 	email citext unique not null,
 	name text not null,
+	phone_number text unique,
+	address text,
 	password_hash bytea not null,
 	created_at timestamp default now(),
 	updated_at timestamp default now(),
